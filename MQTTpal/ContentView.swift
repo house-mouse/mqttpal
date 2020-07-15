fcea49cafcadd0ddcf3bee0ad53643ddea589d3c
Jonathan Stark <llvm@corp.stark.org>
2020-07-14 17:51:55 -0700

    Initial Commit

---
+++ MQTTpal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MQTTpal
+//
+//  Created by Jonathan Stark on 7/14/20.
+//  Copyright © 2020 Jonathan Stark. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
